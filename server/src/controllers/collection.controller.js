7054e4cf3070343a90969aecd6dc2122d3a091be
modala tarun kumar <modalatarunkumar@gmail.com>
2025-10-07 13:44:28 +0530

    minor changes in collection

--- server/src/controllers/collection.controller.js
+++ server/src/controllers/collection.controller.js
@@ -8,6 +8,11 @@ export const createCollection = asyncHandler(async (req, res) => {
     if(!name){
         throw new CustomError("Collection name is required", 401);
     }
+    // check if collection already exists
+    const existingCollection = await Collection.findOne({name})
+    if(existingCollection){
+        throw new CustomError("Collection already exists");
+    }
 
     const collection = await Collection.create({
         name
@@ -23,26 +28,26 @@ export const createCollection = asyncHandler(async (req, res) => {
 export const updateCollection = asyncHandler(async (req, res) => {
     const {name} = req.body;
     const {id: collectionId} = req.params;
-
+    
     if(!name){
         throw new CustomError("Please enter a collection name to update");
     }
-    let udpatedCollection = await Collection.findByIdAndUpdate(collectionId, {
+    let updatedCollection = await Collection.findByIdAndUpdate(collectionId, {
         name
     }, {
         new:true,
         runValidators:true,
     })
 
-    if(!udpatedCollection){
+    if(!updatedCollection){
         throw new CustomError("Collection not found", 400);
     }
 
 
-    res.status(200).message({
+    res.status(200).json({
         success: true,
         message: "Collection updated successfully",
-        updateCollection
+        updatedCollection
     })
 })
 
@@ -50,26 +55,28 @@ export const deleteCollection = asyncHandler(async (req, res) =>{
     const {id:collectionId} = req.params;
 
     const collectionToDelete = await Collection.findById(collectionId)
-    
+    console.log("collection", collectionToDelete)
     if(!collectionToDelete){
         throw new CustomError("Collection not found", 400);
     }
 
-    await collectionToDelete.remove()
+    // await collectionToDelete.remove() remove method is deprecated
+    await collectionToDelete.deleteOne();
+    // also use await Collection.findByIdAndDelete(collectionId)
     
-    res.status(200).message({
+    res.status(200).json({
         success:true,
         message: "Collection deleted successfully"
     })
 })
 
-export const getAllCollection = asyncHandler(async(req, res) => {
+export const getAllCollection = asyncHandler(async (_req, res) => {
     const collections = await Collection.find({})
     
     if(!collections){
         throw new CustomError("there is no collections", 400);
     }
-    res.status(200).message({
+    res.status(200).json({
         success:true,
         collections
     })
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import authRoutes from "./auth.route.js";
-
+import collectionRoutes from "./collection.route.js";
 
 const router = Router();
 router.use("/auth", authRoutes)
+router.use("/collection", collectionRoutes)
 
 
 
